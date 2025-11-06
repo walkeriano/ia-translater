52f3fc74175af059304d46d125bdfa3d84221b44
walkeriano <admin@MACBOOKPRO-6814.station>
2025-11-06 20:10:50 +0100

    cambios listos activados

--- src/components/titleGenerator/TitleGenerator.jsx
+++ src/components/titleGenerator/TitleGenerator.jsx
@@ -1,7 +1,19 @@
+import styles from "./TitleGenerator.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
 export default function TitleGenerator() {
   return (
     <section className={styles.titleGenerator}>
-      <h2>hello world</h2>
+      <h2>traductor de señas con I.A</h2>
+      <div className={styles.containerFlex}>
+        <p>inicia ahora</p>
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          size="1x"
+          className={styles.icon}
+        />
+      </div>
     </section>
   );
 }
